902ae8fe61a21cdb515ca4f40e605725f8b96334
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2022-01-19 16:13:48 +0700

    use @nestjs/config (.env)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { StudentModule } from 'src/student/student.module';
 import { TeacherModule } from 'src/teacher/teacher.module';
 
 @Module({
   imports: [
     StudentModule, 
-    TeacherModule
+    TeacherModule,
+    ConfigModule.forRoot()
   ]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
+const port = process.env.SERVER_LISTEN_PORT;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3399);
+  await app.listen(parseInt(port), () => {
+    console.log(`server running on port ${port}`);
+  })
 }
 bootstrap();
